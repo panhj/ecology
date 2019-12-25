c9e3ff14b519b55c34bffb406e4f58239563ceeb
panhj <1159688878@qq.com>
2019-12-26 02:31:10 +0800

    map gpstopixel

--- src/components/RiverMap.vue
+++ src/components/RiverMap.vue
@@ -13,17 +13,30 @@
 import BMap from 'BMap'
 import BMAP_NORMAL_MAP from 'BMAP_NORMAL_MAP'
 import BMAP_HYBRID_MAP from 'BMAP_HYBRID_MAP'
+import GPSDATA from './gps.js'
 let myMap = null;
 export default {
   name: 'RiverMap',
   data () {
     return {
-      
+      zomm2edge: {
+        "5": 1,
+        "6": 1.2,
+        "7": 1.8,
+        "8": 3,
+        "9": 6,
+        "10": 8,
+        "11": 16,
+        "12": 25
+      }
     }
   },
   methods: {
     initMap () {
-      myMap = new BMap.Map("MAP")
+      myMap = new BMap.Map("MAP", {
+        minZoom: 5,
+        maxZoom: 12
+      })
       const point = new BMap.Point(105, 34)
       myMap.centerAndZoom(point, 6)
       myMap.enableScrollWheelZoom(true)
@@ -32,50 +45,67 @@ export default {
       }))
       myMap.setMapType(BMAP_HYBRID_MAP)
     },
-    draw () {
+    draw (GPSDATA) {
       const self = this
       const canvasLayer = new BMap.CanvasLayer({
         update: function () {
-          self.drawLiner(this.canvas)
+          // this.canvas.style.opacity = 0.5
+          self.drawLiner(this.canvas, GPSDATA)
         }
       })
       myMap.addOverlay(canvasLayer)
     },
-    drawLiner (canvas) {
-      var ctx = canvas.getContext("2d");
+    drawLiner (canvas, GPSDATA) {
+      const EDGE = this.getEdgeFromZoom()
 
-        if (!ctx) {
-            return;
-        }
+      const ctx = canvas.getContext("2d")
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+      // ctx.lineWidth = 10
+      // ctx.strokeStyle = "#ff7788"
+      ctx.beginPath()
 
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+      // const first_gps = GPSDATA[0]
+      // const first_p = new BMap.Point(first_gps[0], first_gps[1])
+      // const first_pixel = myMap.pointToPixel(first_p)
+      // ctx.moveTo(first_pixel.x, first_pixel.y)
 
-        var temp = {};
-        ctx.fillStyle = "rgba(50, 50, 255, 0.7)";
-        ctx.beginPath();
-        var data = [
-        new BMap.Point(116.297047,39.979542),
-        new BMap.Point(116.321768,39.88748),
-        new BMap.Point(116.494243,39.956539)
-        ];
+      // for (let i=1, len=GPSDATA.length; i<len; i++) {
+      //   const gps = GPSDATA[i]
+      //   const p = new BMap.Point(gps[0], gps[1])
+      //   const pixel = myMap.pointToPixel(p)
+      //   ctx.lineTo(pixel.x, pixel.y)
+      // }
+      // ctx.stroke()
 
-        for (var i = 0, len = data.length; i < len; i++) {
-
-            // 绘制时需要对经纬度进行转换
-            var pixel = myMap.pointToPixel(data[i]);
-
-            ctx.fillRect(pixel.x, pixel.y, 30, 30);
-        }
+      ctx.fillStyle = "#ffff0030"
+      for (let i=1, len=GPSDATA.length; i<len; i++) {
+        const gps = GPSDATA[i]
+        const p = new BMap.Point(gps[0], gps[1])
+        const pixel = myMap.pointToPixel(p)
+        ctx.fillRect(pixel.x, pixel.y, EDGE, EDGE);
+      }
     },
     getLineData () {
       this.$http.get('earth-engine/data/2011').then(res => {
-        console.log(JSON.parse(res.message))
+        const { features } = JSON.parse(res.message)
+        const gps_arr = features[0].geometry.coordinates[0][0]
+        const gpsData = gps_arr.filter((v, i) => i % 10 === 0)
+        const m_len = (gpsData.length / 2)>>0
+        const point = new BMap.Point(gpsData[m_len][0], gpsData[m_len][1])
+        myMap.setCenter(point)
+        myMap.setZoom(9)
+        this.draw(gpsData)
       })
+    },
+    getEdgeFromZoom () {
+      const zoom = myMap.getZoom()
+      let EDGE = 2
+      return this.zomm2edge[zoom+''] || 1
     }
   },
   mounted () {
     this.initMap()
-    this.draw()
+    // this.draw(GPSDATA)
     this.getLineData()
   }
 }
